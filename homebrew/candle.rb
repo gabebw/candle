c005506b54710c6e937b5f77e2e0baf1db0a9b70
Gabe Berke-Williams <gabebw@gabebw.com>
2019-08-30 22:27:37 -0700

    Update Homebrew formula for v0.2.0

--- homebrew/candle.rb
+++ homebrew/candle.rb
@@ -1,10 +1,10 @@
 class Candle < Formula
-  version "0.1.0"
+  version "0.2.0"
   desc "Shine a little light on your HTML using the command line"
   homepage "https://github.com/gabebw/candle"
 
   url "https://github.com/gabebw/candle/releases/download/v#{version}/candle-#{version}.tar.gz"
-  sha256 "ed4e1bfd63b935624413abbfc0bcac4a89a5e0788302bd9c26e76b0b17685d85"
+  sha256 "fdf90f2fa96c30fb8f62768cc755cef69793d1880e810234f9fc0f2857f2c3eb"
 
   def install
     bin.install "candle"
