e18eb49175ca6ae3ad688ff8827a666884b0acee
Gabe Berke-Williams <gabebw@gabebw.com>
2019-09-05 18:50:49 -0700

    Check for `Some(...)` earlier in app flow
    Now `print_tree` won't be called at all if there's no element.

--- src/main.rs
+++ src/main.rs
@@ -46,47 +46,47 @@ const SELF_CLOSING_ELEMENTS: &[&str; 16] = &[
     "wbr"
 ];
 
-fn print_tree(element: Option<ElementRef>, indent_level: usize) -> String {
+fn print_tree(element: ElementRef, indent_level: usize) -> String {
     let mut s = String::new();
     let indent = format!("{:n$}", "", n=(indent_level * NUMBER_OF_SPACES_PER_LEVEL));
     let indent_plus_one = format!("{:n$}", "", n=(indent_level + 1) * NUMBER_OF_SPACES_PER_LEVEL);
-    if let Some(element) = element {
-        // `element` is https://docs.rs/scraper/0.10.1/scraper/element_ref/struct.ElementRef.html
-        let top = element.value();
-        let tag_name: &str = &format!("{}", top.name.local);
+    // `element` is https://docs.rs/scraper/0.10.1/scraper/element_ref/struct.ElementRef.html
+    let top = element.value();
+    let tag_name: &str = &format!("{}", top.name.local);
 
-        if SELF_CLOSING_ELEMENTS.contains(&tag_name) {
-            // The tag is self-closing and can't have any children
-            s.push_str(&format!("{}{:?}</{}>", indent, top, tag_name));
-        } else {
-            // Opening tag, with attributes
-            s.push_str(&format!("{}{:?}", indent, top));
+    if SELF_CLOSING_ELEMENTS.contains(&tag_name) {
+        // The tag is self-closing and can't have any children
+        s.push_str(&format!("{}{:?}</{}>", indent, top, tag_name));
+    } else {
+        // Opening tag, with attributes
+        s.push_str(&format!("{}{:?}", indent, top));
 
-            for child in element.children() {
-                // Each `child` is a NodeRef<Node>:
-                // https://docs.rs/ego-tree/0.3.0/ego_tree/struct.NodeRef.html
-                // https://docs.rs/scraper/0.10.1/scraper/node/enum.Node.html
-                match child.value() {
-                    Node::Comment(c) => {
-                        s.push_str(&format!("\n{}<!-- {} -->", indent_plus_one, c.comment.trim()))
-                    },
-                    Node::Element(_) => {
+        for child in element.children() {
+            // Each `child` is a NodeRef<Node>:
+            // https://docs.rs/ego-tree/0.3.0/ego_tree/struct.NodeRef.html
+            // https://docs.rs/scraper/0.10.1/scraper/node/enum.Node.html
+            match child.value() {
+                Node::Comment(c) => {
+                    s.push_str(&format!("\n{}<!-- {} -->", indent_plus_one, c.comment.trim()))
+                },
+                Node::Element(_) => {
+                    if let Some(element) = ElementRef::wrap(child) {
                         s.push_str("\n");
-                        s.push_str(&print_tree(ElementRef::wrap(child), indent_level + 1));
-                    },
-                    Node::Text(t) => {
-                        // Ignore this text if it's just whitespace
-                        if ! t.text.trim().is_empty() {
-                            s.push_str(&format!("\n{}{}", indent_plus_one, t.text))
-                        }
-                    },
-                    _ => {},
-                }
+                        s.push_str(&print_tree(element, indent_level + 1));
+                    }
+                },
+                Node::Text(t) => {
+                    // Ignore this text if it's just whitespace
+                    if ! t.text.trim().is_empty() {
+                        s.push_str(&format!("\n{}{}", indent_plus_one, t.text))
+                    }
+                },
+                _ => {},
             }
-
-            // Closing tag
-            s.push_str(&format!("\n{}</{}>", indent, tag_name));
         }
+
+        // Closing tag
+        s.push_str(&format!("\n{}</{}>", indent, tag_name));
     }
     s
 }
@@ -96,7 +96,7 @@ impl<'a> Finder<'a> {
         match self.operation {
             FinderOperation::Text => Some(element.text().collect()),
             FinderOperation::Attr(attr) => element.value().attr(attr).map(|s| s.to_string()),
-            FinderOperation::Html => Some(print_tree(Some(*element), 0))
+            FinderOperation::Html => Some(print_tree(*element, 0))
         }
     }
 }
